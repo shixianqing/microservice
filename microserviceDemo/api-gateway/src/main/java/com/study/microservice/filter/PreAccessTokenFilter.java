b5452c2f7c7747c385aabcef9a9caed0bfc09ddb
shixianqing <1434501995@qq.com>
2019-08-25 20:38:46 +0800

    add

---
+++ microserviceDemo/api-gateway/src/main/java/com/study/microservice/filter/PreAccessTokenFilter.java
@@ -0,0 +1,98 @@
+package com.study.microservice.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * *                            _ooOoo_
+ * *                           o8888888o
+ * *                           88" . "88
+ * *                           (| -_- |)
+ * *                            O\ = /O
+ * *                        ____/`---'\____
+ * *                      .   ' \\| |// `.
+ * *                       / \\||| : |||// \
+ * *                    / _||||| -:- |||||- \
+ * *                       | | \\\ - /// | |
+ * *                     | \_| ''\---/'' | |
+ * *                      \ .-\__ `-` ___/-. /
+ * *                   ___`. .' /--.--\ `. . __
+ * *                ."" '< `.___\_<|>_/___.' >'"".
+ * *               | | : `- \`.;`\ _ /`;.`/ - ` : | |
+ * *                 \ \ `-. \_ __\ /__ _/ .-` / /
+ * *         ======`-.____`-.___\_____/___.-`____.-'======
+ * *                            `=---='
+ * *
+ * *         .............................................
+ * *                  佛祖保佑             永无BUG
+ * *          佛曰:
+ * *                  写字楼里写字间，写字间里程序员；
+ * *                  程序人员写程序，又拿程序换酒钱。
+ * *                  酒醒只在网上坐，酒醉还来网下眠；
+ * *                  酒醉酒醒日复日，网上网下年复年。
+ * *                  但愿老死电脑间，不愿鞠躬老板前；
+ * *                  奔驰宝马贵者趣，公交自行程序员。
+ * *                  别人笑我忒疯癫，我笑自己命太贱；
+ * *                  不见满街漂亮妹，哪个归得程序员？
+ *
+ * @Author:shixianqing
+ * @Date:2019/8/23 15:26
+ * @Description:
+ **/
+@Component
+public class PreAccessTokenFilter extends ZuulFilter {
+
+    /**
+     * 过滤器类型
+     * 决定过滤器在请求的哪个生命周期中执行
+     * pre：表示会在路由之前执行
+     * @return
+     */
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    /**
+     * 过滤器顺序
+     * @return
+     */
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+
+    /**
+     * 表示过滤器是否需要被执行
+     * @return
+     */
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        /**
+         * 拦截请求，判断请求头里是否有携带token
+         */
+
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        String accesstoken = request.getHeader("accesstoken");
+        if (StringUtils.isEmpty(accesstoken)){
+            //令zuul过滤该请求，不对其进行路由
+            requestContext.setSendZuulResponse(false);
+            requestContext.setResponseStatusCode(401);
+        }
+        return null;
+    }
+}
+
+
